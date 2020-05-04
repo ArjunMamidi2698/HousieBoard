cee5dffca6d2aa79f84d5e12dcac051186c1fb5a
Mallikharjuna <mallikharjuna.mamidi@4iq.com>
2020-05-04 19:40:49 +0530

    added config vars

--- backend/database/mongoConnectURI.js
+++ backend/database/mongoConnectURI.js
@@ -1,11 +1,10 @@
-var authentication = require('./config.json');
+require('dotenv').config()
 
-var username = authentication.username;
-var password = authentication.password;
-var host = authentication.host;
-var databaseName = authentication.databaseName;
+var username = process.env.DBUSERNAME;
+var password = process.env.PASSWORD;
+var host = process.env.HOST;
+var databaseName = process.env.DATABASENAME;
 
-var uri = 'mongodb+srv://'+username+':'+password+'@'+host+'/'+databaseName+'?retryWrites=true&w=majority';
-
+var uri = `mongodb+srv://${username}:${password}@${host}/${databaseName}?retryWrites=true&w=majority`;
 
 module.exports.uri=uri;
